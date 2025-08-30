2bc8ed281ed489540f32c9551d1a3653e80ea158
abrarasif11 <faasif929@gmail.com>
2025-08-30 08:49:59 +0600

    server-not-running-error-fixed

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 6969;
+const port = process.env.PORT || 5000;
 
-//Middleware 
-app.use(cors);
-app.use(express.json())
+// Middleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send("emporium espresso server is running");
+});
+
+app.listen(port, () => {
+    console.log(`Emporium Espresso Server is running on PORT:${port}`)
+});
