1550d17a94614ece631e5bbbbfdcfe20e527a0a7
abrarasif11 <faasif929@gmail.com>
2025-09-01 23:33:07 +0600

    user-delete

--- index.js
+++ index.js
@@ -86,12 +86,24 @@ app.delete('/coffee/:id', async(req, res) => {
 })
 
 // users API 
+app.get('/user', async(req,res) => {
+  const cursor = userCollection.find();
+  const result = await cursor.toArray()
+  res.send(result);
+})
 
 app.post('/user', async(req,res) => {
  const newUser = req.body;
  console.log("Creating New User", newUser);
  const result = await userCollection.insertOne(newUser);
  res.send(result);
+})
+
+app.delete('user/:id', async (req, res) =>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id) }
+  const result = await userCollection.deleteOne(query);
+  res.send(result);
 })
 
     // Send a ping to confirm a successful connection
